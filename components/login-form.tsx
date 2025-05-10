55be5c9b6e2e45f73b2f03be19dabdcb7ced8ad0
Akib306 <123131218+Akib306@users.noreply.github.com>
2025-05-09 21:17:15 -0600

    feat: add a divider in the login form

--- components/login-form.tsx
+++ components/login-form.tsx
@@ -90,7 +90,16 @@ export function LoginForm({ className, ...props }: React.ComponentPropsWithoutRe
               </Button>
             </div>
 
-            <OAuthButtons className='flex flex-col mt-4 w-full' />
+            <div className="relative my-4">
+              <div className="absolute inset-0 flex items-center">
+                <span className="w-full border-t" />
+              </div>
+              <div className="relative flex justify-center text-xs uppercase">
+                <span className="bg-background px-2 text-muted-foreground">or</span>
+              </div>
+            </div>
+
+            <OAuthButtons className='flex flex-col w-full' />
 
             <div className="mt-4 text-center text-sm">
               Don&apos;t have an account?{' '}
