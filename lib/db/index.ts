a8dd52e4b21070cd935e5ed14d5a48776265a9a2
kararAcode <63989704+kararAcode@users.noreply.github.com>
2025-06-15 14:56:00 -0600

    profile creation on signup

--- lib/db/index.ts
+++ lib/db/index.ts
@@ -1,8 +1,8 @@
-import * as users from "./users";
+import * as profiles from "./profiles";
 import * as subscriptions from "./subscriptions";
 import * as messages from "./messages";
 import * as chats from "./chats";
 import * as api_usage from "./api_usage";
 
-export { users, subscriptions, messages, chats, api_usage };
+export { profiles, subscriptions, messages, chats, api_usage };
 
