b032c5b9c482899384df3f328c846729fc401202
kararAcode <63989704+kararAcode@users.noreply.github.com>
2025-06-21 16:07:22 -0600

    create client side supabase utils

--- lib/db/index.ts
+++ lib/db/index.ts
@@ -1,8 +1,10 @@
-import * as profiles from "./profiles";
-import * as subscriptions from "./subscriptions";
-import * as messages from "./messages";
-import * as chats from "./chats";
-import * as api_usage from "./api_usage";
 
-export { profiles, subscriptions, messages, chats, api_usage };
+// Server-side utilities (for API routes, server components)
+export * as profiles from './profiles';
+export * as chats from './chats';
+export * as messages from './messages';
+export * as subscriptions from './subscriptions';
+export * as api_usage from './api_usage';
+
+// Client-side utilities (for client components)
 
