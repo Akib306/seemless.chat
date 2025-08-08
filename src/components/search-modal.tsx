7386deed43688836015709fd5f406c2e6253e370
Akib306 <123131218+Akib306@users.noreply.github.com>
2025-08-08 14:14:16 -0600

    style: larger kbd

--- src/components/search-modal.tsx
+++ src/components/search-modal.tsx
@@ -166,7 +166,7 @@ export function SearchModal({ collapsed = false }: SearchModalProps){
 					</div>
 					
 					<kbd className="pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5">
-						<span className="text-xs">⌘</span>K
+						<span className="text-base">⌘+K</span>
 					</kbd>
 				</Button>
 			)}
@@ -214,7 +214,7 @@ export function SearchModal({ collapsed = false }: SearchModalProps){
             </CommandGroup>
           )}
           {/* Sticky helper footer */}
-       
+      
         </CommandList>
       </CommandDialog>
 		</>
