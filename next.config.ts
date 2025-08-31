452465f99b266090487f11347887a0aff6874dc7
Akib306 <123131218+Akib306@users.noreply.github.com>
2025-08-30 23:57:15 -0600

    feat: add bundle analyzer configuration

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,5 @@
 import type { NextConfig } from "next";
+import withBundleAnalyzer from "@next/bundle-analyzer";
 
 const nextConfig: NextConfig = {
 	reactStrictMode: true,
@@ -19,4 +20,6 @@ const nextConfig: NextConfig = {
 	},
 };
 
-export default nextConfig;
+export default withBundleAnalyzer({
+	enabled: process.env.ANALYZE === "true",
+})(nextConfig);
