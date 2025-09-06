29656ef0c2abca0e96f25d38420b15c8c8ac2c96
Akib306 <123131218+Akib306@users.noreply.github.com>
2025-09-06 00:04:11 -0600

    feat: add AppBackground component

---
+++ src/components/app-background.tsx
@@ -0,0 +1,12 @@
+import BrandBg from "@/components/brand-bg";
+import { InteractiveBackground } from "@/components/interactive-bg";
+
+export default function AppBackground() {
+  return (
+    <>
+      <BrandBg />
+      <InteractiveBackground />
+    </>
+  );
+}
+
